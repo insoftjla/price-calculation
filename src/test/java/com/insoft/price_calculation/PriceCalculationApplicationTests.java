472b55cef4211ced828e2dd77399d7697e385431
insoftjla <sinyakin@innoseti.com>
2024-06-22 16:32:32 +0300

    init app

---
+++ src/test/java/com/insoft/price_calculation/PriceCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insoft.price_calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
